ec357f22d0b070858754441cd92644554599a918
Jeffrey Hechler <jeffrey.hechler@gmail.com>
2016-11-10 18:41:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  answer = ""
+  i = 0
+  if array.length == 1
+    answer = array[i]
+    return answer
+  elsif array.length == 2
+    answer = "#{array[i]} and #{array[i+1]}"
+    return answer
+  else
+    while i < array.length - 1
+      answer = answer + "#{array[i]}, "
+      i += 1
+    end
+    answer = answer + "and #{array[i]}"
+    return answer
+  end
 end
